46abe515f89da984138ac90f758321dd3b32acd5
saishkulkarni <saishkulkarni7@gmail.com>
2025-10-07 16:03:23 +0530

    1. Project Creation

---
+++ src/main/java/com/jsp/clinkNBuy/ClinkNBuyRestApplication.java
@@ -0,0 +1,13 @@
+package com.jsp.clinkNBuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinkNBuyRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinkNBuyRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsp/clinkNBuy/ClinkNBuyRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp.clinkNBuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinkNBuyRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
