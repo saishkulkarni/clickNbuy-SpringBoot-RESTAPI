15150499fb9a6762610e06a727655143a4824252
saishkulkarni <saishkulkarni7@gmail.com>
2025-10-17 16:47:55 +0530

    12. CI Check

--- src/test/java/com/jsp/clinkNBuy/ClinkNBuyRestApplicationTests.java
+++ src/test/java/com/jsp/clinkNBuy/ClinkNBuyRestApplicationTests.java
@@ -1,13 +1,8 @@
 package com.jsp.clinkNBuy;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ClinkNBuyRestApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
